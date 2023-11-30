7b210c304de7c46c1619fd8b9349011729a83696
MiftahN <97022981+RedSafir@users.noreply.github.com>
2023-11-30 22:38:54 +0700

    still working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,11 @@ dependencies {
 /*    implementation ("com.google.dagger:hilt-android:2.44.2")
     ksp("com.google.dagger:hilt-android-compiler:2.44.2")
 
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     ksp("androidx.hilt:hilt-compiler:1.1.0")*/
+
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
-
     kapt("androidx.hilt:hilt-compiler:1.1.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Support
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
